a4a8204c270310983095df3d96cf56584c741d39
MukendiMputu <mputusam@live.com>
2020-02-28 14:22:23 -0600

    used urlencode and rawurlencode
    Signed-off-by: MukendiMputu <mputusam@live.com>

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,4 +1,9 @@
 <?php
-    $id = $_GET['id'];
+    //$id = isset($_GET['id']) ? $_GET['id'] : '1';
+    $id = $_GET['id'] ?? '1'; // since PHP 7.0 and later
     echo $id;
 ?>
+
+<a href="staff/subjects/show.php?name=<?php echo urlencode('John Doe'); ?>">Link</a><br />
+<a href="staff/subjects/show.php?company=<?php echo urlencode('Widgets&More'); ?>">Link</a><br />
+<a href="staff/subjects/show.php?query=<?php echo urlencode('!#*?'); ?>">Link</a><br />
