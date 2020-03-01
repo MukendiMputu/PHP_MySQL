acecc7f7751dffd7e3e95a6eb4cb4721d6b18e77
MukendiMputu <mputusam@live.com>
2020-03-01 00:51:58 -0600

    connection and query error handling
    Signed-off-by: MukendiMputu <mputusam@live.com>

--- private/query_functions.php
+++ private/query_functions.php
@@ -5,6 +5,7 @@
         $sql = "SELECT * FROM subjects ";
         $sql .= "ORDER BY position ASC";
         $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
         return $result;
     }
 ?>
