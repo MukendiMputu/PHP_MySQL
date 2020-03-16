7e1a6654f3cefdd15b33932b7d53d4c8e910c88b
MukendiMputu <mputusam@live.com>
2020-03-16 16:32:30 -0600

    fixed IDOR
    Signed-off-by: MukendiMputu <mputusam@live.com>

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,8 +1,13 @@
 <?php
-    function find_all_subjects() {
+    function find_all_subjects($options=[]) {
         global $db;
 
+        $visible = $options['visible'] ?? false;
+
         $sql = "SELECT * FROM subjects ";
+        if($visible){
+            $sql .= "WHERE visible = true ";
+        }
         $sql .= "ORDER BY position ASC";
         $result = mysqli_query($db, $sql);
         confirm_result_set($result);
@@ -19,11 +24,16 @@
         return $result;
     }
 
-    function find_page_by_id($page_id) {
+    function find_page_by_id($page_id, $options=[]) {
         global $db;
 
+        $visible = $options['visible'] ?? false;
+
         $sql = "SELECT * FROM pages ";
-        $sql .= "WHERE id ='" . db_escape($db, $page_id) . "'";
+        $sql .= "WHERE id ='" . db_escape($db, $page_id) . "' ";
+        if ($visible) {
+            $sql .= "AND visible = true ";
+        }
         $result = mysqli_query($db, $sql);
         confirm_result_set($result);
         $page = mysqli_fetch_assoc($result);
@@ -31,22 +41,32 @@
         return $page;
     }
 
-    function find_pages_by_subject_id ($subject_id) {
+    function find_pages_by_subject_id ($subject_id, $options=[]) {
         global $db;
+
+        $visible = $options['visible'] ?? false;
 
         $sql = "SELECT * FROM pages ";
         $sql .= "WHERE subject_id='" . db_escape($db, $subject_id) . "' ";
+        if ($visible) {
+            $sql .= "AND visible = true ";
+        }
         $sql .= "ORDER BY position ASC";
         $result = mysqli_query($db, $sql);
         confirm_result_set($result);
         return $result;
     }
 
-    function find_subject_by_id($subject_id) {
+    function find_subject_by_id($subject_id, $options=[]) {
         global $db;
 
+        $visible = $options['visible'] ?? false;
+
         $sql = "SELECT * FROM subjects ";
-        $sql .= "WHERE id ='" . db_escape($db, $subject_id) . "'";
+        $sql .= "WHERE id ='" . db_escape($db, $subject_id) . "' ";
+        if ($visible) {
+            $sql .= "AND visible = true ";
+        }
         $result = mysqli_query($db, $sql);
         confirm_result_set($result);
         $subject = mysqli_fetch_assoc($result);
--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -1,12 +1,12 @@
 <navigation>
-    <?php $nav_subjects = find_all_subjects(); ?>
+    <?php $nav_subjects = find_all_subjects(['visible' => true]); ?>
     <ul class="subjects">
         <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
         <li class="<?php echo $nav_subject['id'] == $subject_id ? 'selected' : '' ?>">
             <a href="<?php echo url_for('index.php?subject_id=' . h(u($nav_subject['id']))); ?>" > <?php echo h($nav_subject['menu_name']);?> </a>
             <?php
                 if($nav_subject['id'] == ($subject_id ?? '')) {
-                 $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
+                 $nav_pages = find_pages_by_subject_id($nav_subject['id'], ['visible' => true]); ?>
                 <ul class="pages">
                     <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
                     <li class="<?php echo $nav_page['id'] == $page_id ? 'selected' : '' ?>">
